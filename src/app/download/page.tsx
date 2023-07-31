c9e702e3732646fea29f13a046f31dc9f9d6740b
aladdinmiles <precious@aladdinmiles.com>
2023-07-31 15:17:44 +0300

    make QRCode functional

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,17 +1,10 @@
-'use client';
-import { detectOS } from '@/utils/detectOS';
+import { Redirect } from './redirect';
 
 const DownloadApp: React.FC = () => {
-  const os = detectOS();
-  if (os == 'iPad' || os == 'iOS' || os == 'Mac OS') {
-    location.replace(
-      'https://apps.apple.com/app/aladdin-miles-%D8%A7%D9%85%D9%8A%D8%A7%D9%84-%D8%B9%D9%84%D8%A7%D8%A1-%D8%A7%D9%84%D8%AF%D9%8A%D9%86/id1503894088'
-    );
-  } else {
-    location.replace(
-      'https://play.google.com/store/apps/details?id=com.aladdinmiles.customer&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'
-    );
-  }
-  return <div></div>;
+  return (
+    <div>
+      <Redirect />
+    </div>
+  );
 };
 export default DownloadApp;
