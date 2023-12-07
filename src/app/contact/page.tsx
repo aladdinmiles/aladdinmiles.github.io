f14d29a268f836ff19aed08d258dd47e07f48742
aladdinmiles <precious@aladdinmiles.com>
2023-12-07 16:20:05 +0100

    support page design

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,6 @@ import {
   Form,
   GetHelp,
   Header,
-  Img,
   InputWrapper,
   Label,
   SideBySide,
@@ -25,45 +24,43 @@ export const metadata: Metadata = {
 export const Contact: React.FC = () => {
   return (
     <Background>
-      {true && (
-        <Form>
-          <TwentyFour>
-            <GetHelp>Get Help</GetHelp>
-            <div>
-              <Header>Need help? Contact us.</Header>
-              <SubHeader>
-                Complete this form to get connected to our support team. We will
-                get back to you shortly.
-              </SubHeader>
-            </div>
-          </TwentyFour>
-          <TwentyFour>
-            <SideBySide>
-              <InputWrapper>
-                <Label>First Name</Label>
-                <TextInput name="first_name" />
-              </InputWrapper>
-              <InputWrapper>
-                <Label>Last Name</Label>
-                <TextInput name="last_name" />
-              </InputWrapper>
-            </SideBySide>
-            <SideBySide>
-              <InputWrapper>
-                <Label>Phone Number</Label>
-                <TextInput name="phone" />
-              </InputWrapper>
-              <InputWrapper>
-                <Label>Email</Label>
-                <TextInput name="email" />
-              </InputWrapper>
-            </SideBySide>
-          </TwentyFour>
-          <Submit href="#">Submit</Submit>
-        </Form>
-      )}
+      <Form>
+        <TwentyFour>
+          <GetHelp>Get Help</GetHelp>
+          <div>
+            <Header>Need help? Contact us.</Header>
+            <SubHeader>
+              Complete this form to get connected to our support team. We will
+              get back to you shortly.
+            </SubHeader>
+          </div>
+        </TwentyFour>
+        <TwentyFour>
+          <SideBySide>
+            <InputWrapper>
+              <Label>First Name</Label>
+              <TextInput name="first_name" />
+            </InputWrapper>
+            <InputWrapper>
+              <Label>Last Name</Label>
+              <TextInput name="last_name" />
+            </InputWrapper>
+          </SideBySide>
+          <SideBySide>
+            <InputWrapper>
+              <Label>Phone Number</Label>
+              <TextInput name="phone" />
+            </InputWrapper>
+            <InputWrapper>
+              <Label>Email</Label>
+              <TextInput name="email" />
+            </InputWrapper>
+          </SideBySide>
+        </TwentyFour>
+        <Submit href="#">Submit</Submit>
+      </Form>
 
-      {false && (
+      {/* {false && (
         <Form>
           <TwentyFour>
             <GetHelp>Get Help</GetHelp>
@@ -83,7 +80,7 @@ export const Contact: React.FC = () => {
           />
           <Submit href="#">Submit</Submit>
         </Form>
-      )}
+      )} */}
     </Background>
   );
 };
