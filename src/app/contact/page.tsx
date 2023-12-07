4edcb46b5b23ea8bd4ab2afd4321ff7986add2fe
aladdinmiles <precious@aladdinmiles.com>
2023-12-07 16:26:16 +0100

    support page design

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -19,10 +19,10 @@ export const metadata: Metadata = {
   description:
     'We are building a magical world. The best place to meet your travel and shopping needs all at once.',
   keywords:
-    'About us, About AladdinMiles, Travel for free, Travel for free KSA, Travel for free Saudi Arabia, Travel for free world wide, merchants, venues, AladdinMiles Portal, Loyalty app, loyalty saudi arabia, airlines loyalty in saudi arabia, loyalty app KSA, AladdinMiles Kingdom'
+    'Contact us, About AladdinMiles, Travel for free, Travel for free KSA, Travel for free Saudi Arabia, Travel for free world wide, merchants, venues, AladdinMiles Portal, Loyalty app, loyalty saudi arabia, airlines loyalty in saudi arabia, loyalty app KSA, AladdinMiles Kingdom'
 };
 
-export const Contact: React.FC = () => {
+const Contact: React.FC = () => {
   return (
     <Background>
       <Form>
