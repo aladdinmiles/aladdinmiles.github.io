cfacb3cb47f2c0caa0f5a2de1ea5259971426c1d
aladdinmiles <precious@aladdinmiles.com>
2023-12-21 00:15:19 +0100

    Deploy

---
+++ out/_next/static/chunks/app/privacy-policy/page-b37a710809313305.js
@@ -0,0 +1,85 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [310],
+  {
+    7691: function (n, e, t) {
+      Promise.resolve().then(t.bind(t, 431)),
+        Promise.resolve().then(t.bind(t, 7422)),
+        Promise.resolve().then(t.bind(t, 3839));
+    },
+    7422: function (n, e, t) {
+      'use strict';
+      t.r(e),
+        t.d(e, {
+          ColoredSection: function () {
+            return f;
+          },
+          HeroContent: function () {
+            return s;
+          },
+          HeroContentTextArea: function () {
+            return u;
+          },
+          MultiStyleText: function () {
+            return d;
+          }
+        });
+      var o = t(230),
+        i = t(6030),
+        c = t.n(i),
+        a = t(7247),
+        l = t(8600);
+      function r() {
+        let n = (0, o._)([
+          '\n  display: flex;\n  flex-direction: column;\n  align-items: flex-start;\n  gap: ',
+          ';\n'
+        ]);
+        return (
+          (r = function () {
+            return n;
+          }),
+          n
+        );
+      }
+      let f = l.Z.section([
+          '\n  display: flex;\n  height: '
+            .concat((0, a.Wk)(434), ';\n  padding: ')
+            .concat((0, a.Wk)(88.5), ' ')
+            .concat(
+              (0, a.so)(100),
+              ';\n  justify-content: center;\n  align-items: center;\n  background: linear-gradient(163deg, #ffebf0 0%, #fff8eb 100%);\n'
+            ),
+          (0, a.wz)({
+            padding: ''.concat((0, a.$s)(40), ' ').concat((0, a.CD)(24))
+          })
+        ]),
+        s = l.Z.div([
+          '\n  display: inline-flex;\n  flex-direction: column;\n  align-items: flex-start;\n  gap: '
+            .concat((0, a.Wk)(40), ';\n  width: ')
+            .concat((0, a.so)(560), ';\n'),
+          (0, a.wz)({})
+        ]),
+        u = l.Z.div(r(), (0, a.Wk)(16)),
+        d = l.Z.h1([
+          c().style,
+          { fontSize: (0, a.JB)(32), color: '#1D2029' },
+          (0, a.wz)({ fontSize: (0, a.jo)(24) })
+        ]);
+    },
+    6030: function (n) {
+      n.exports = {
+        style: {
+          fontFamily: "'__Raleway_25e0a3', '__Raleway_Fallback_25e0a3'",
+          fontWeight: 700,
+          fontStyle: 'normal'
+        },
+        className: '__className_25e0a3'
+      };
+    }
+  },
+  function (n) {
+    n.O(0, [641, 36, 971, 596, 744], function () {
+      return n((n.s = 7691));
+    }),
+      (_N_E = n.O());
+  }
+]);
