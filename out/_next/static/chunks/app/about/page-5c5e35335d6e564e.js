66a05531d827fb96eb878c6bf13a11cdc9e76bd5
aladdinmiles <precious@aladdinmiles.com>
2023-12-20 23:31:24 +0100

    Deploy

---
+++ out/_next/static/chunks/app/about/page-5c5e35335d6e564e.js
@@ -0,0 +1,309 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [301],
+  {
+    5315: function (e, n, t) {
+      Promise.resolve().then(t.t.bind(t, 3222, 23)),
+        Promise.resolve().then(t.bind(t, 6773)),
+        Promise.resolve().then(t.bind(t, 431)),
+        Promise.resolve().then(t.bind(t, 3839)),
+        Promise.resolve().then(t.bind(t, 750)),
+        Promise.resolve().then(t.bind(t, 5297));
+    },
+    5297: function (e, n, t) {
+      'use strict';
+      t.r(n),
+        t.d(n, {
+          BlueBox: function () {
+            return h;
+          },
+          BoxArea: function () {
+            return u;
+          },
+          HeroLeft: function () {
+            return f;
+          },
+          HeroRight: function () {
+            return d;
+          },
+          HeroSection: function () {
+            return a;
+          },
+          HistoryContent: function () {
+            return x;
+          },
+          HistoryHeaderSection: function () {
+            return b;
+          },
+          HistorySection: function () {
+            return p;
+          },
+          JoinUs: function () {
+            return m;
+          },
+          JoinUsContent: function () {
+            return w;
+          },
+          JoinUsContentTextArea: function () {
+            return k;
+          },
+          JoinUsHeader: function () {
+            return C;
+          },
+          PinkBox: function () {
+            return g;
+          },
+          RaleTextWhite: function () {
+            return v;
+          }
+        });
+      var o = t(230),
+        i = t(7247),
+        r = t(8600),
+        c = t(431);
+      function s() {
+        let e = (0, o._)([
+          '\n  display: inline-flex;\n  flex-direction: column;\n  align-items: flex-start;\n  gap: ',
+          ';\n'
+        ]);
+        return (
+          (s = function () {
+            return e;
+          }),
+          e
+        );
+      }
+      function l() {
+        let e = (0, o._)([
+          '\n  display: flex;\n  flex-direction: column;\n  align-items: flex-start;\n  gap: ',
+          ';\n'
+        ]);
+        return (
+          (l = function () {
+            return e;
+          }),
+          e
+        );
+      }
+      let a = r.Z.section({
+          backgroundColor: '#FFF5F8',
+          padding: ''.concat((0, i.Wk)(60), ' ').concat((0, i.so)(100)),
+          justifyContent: 'space-between',
+          display: 'flex',
+          alignItems: 'center',
+          ...(0, i.wz)({
+            flexDirection: 'column',
+            padding: ''.concat((0, i.$s)(40), ' ').concat((0, i.CD)(24)),
+            justifyContent: 'center'
+          })
+        }),
+        f = r.Z.div({
+          display: 'flex',
+          flexDirection: 'column',
+          gap: (0, i.Wk)(16),
+          width: (0, i.so)(560),
+          ...(0, i.wz)({
+            width: '100%',
+            alignItems: 'center',
+            textAlign: 'center'
+          })
+        }),
+        d = r.Z.div({
+          display: 'flex',
+          gap: (0, i.so)(24),
+          '& .phone': {
+            width: (0, i.so)(300),
+            height: (0, i.Wk)(600),
+            objectFit: 'cover',
+            objectPosition: 'center'
+          },
+          ...(0, i.wz)({
+            '& .phone': {
+              width: (0, i.CD)(300),
+              height: (0, i.$s)(600),
+              objectFit: 'cover',
+              objectPosition: 'center'
+            }
+          })
+        }),
+        u = r.Z.div({
+          display: 'flex',
+          flexDirection: 'column',
+          gap: (0, i.Wk)(20),
+          ...(0, i.wz)({ display: 'none' })
+        }),
+        g = r.Z.div({
+          width: (0, i.so)(300),
+          height: (0, i.Wk)(290),
+          flexShrink: 0,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#FEE6F9',
+          borderRadius: (0, i.so)(16),
+          '& img': {
+            width: (0, i.so)(160),
+            height: (0, i.so)(160),
+            objectFit: 'cover',
+            objectPosition: 'center'
+          }
+        }),
+        h = r.Z.div({
+          width: (0, i.so)(300),
+          height: (0, i.Wk)(290),
+          flexShrink: 0,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#9CBEFC',
+          borderRadius: (0, i.so)(16),
+          '& img': {
+            width: (0, i.so)(160),
+            height: (0, i.so)(160),
+            objectFit: 'cover',
+            objectPosition: 'center'
+          }
+        }),
+        p = r.Z.div({
+          backgroundColor: '#EBF2FF',
+          display: 'flex',
+          flexDirection: 'row',
+          gap: (0, i.so)(40),
+          padding: ''.concat((0, i.Wk)(100), ' ').concat((0, i.so)(80)),
+          alignItems: 'center',
+          '& img': {
+            width: (0, i.so)(600),
+            height: (0, i.so)(500),
+            objectFit: 'cover',
+            objectPosition: 'center',
+            borderRadius: (0, i.so)(16)
+          },
+          ...(0, i.wz)({
+            flexDirection: 'column-reverse',
+            gap: (0, i.$s)(24),
+            '& img': {
+              width: (0, i.CD)(366),
+              height: (0, i.$s)(240),
+              objectFit: 'cover',
+              objectPosition: 'center',
+              borderRadius: (0, i.CD)(8)
+            },
+            padding: ''.concat((0, i.$s)(40), ' ').concat((0, i.CD)(24)),
+            '& h1': { fontSize: (0, i.CD)(24) }
+          })
+        }),
+        b = r.Z.div({ ...(0, i.wz)({ gap: (0, i.$s)(24) }) }),
+        x = r.Z.div([
+          '\n  display: inline-flex;\n  flex-direction: column;\n  align-items: flex-start;\n  gap: '.concat(
+            (0, i.Wk)(24),
+            ';\n'
+          ),
+          (0, i.wz)({ gap: (0, i.$s)(24) })
+        ]),
+        m = r.Z.section({
+          background: 'url(/images/blueBg.png), no-repeat',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          padding: ''.concat((0, i.Wk)(157), ' ').concat((0, i.so)(100)),
+          minHeight: (0, i.Wk)(760),
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          '& img': {
+            width: (0, i.so)(400),
+            height: (0, i.so)(480),
+            objectFit: 'cover',
+            objectPosition: 'center'
+          },
+          ...(0, i.wz)({
+            '& img': { display: 'none' },
+            padding: ''.concat((0, i.$s)(40), ' ').concat((0, i.CD)(24))
+          })
+        }),
+        w = r.Z.div(s(), (0, i.Wk)(40)),
+        k = r.Z.div(l(), (0, i.Wk)(16)),
+        C = (0, r.Z)(c.MultiStyleText)({
+          color: '#ffffff',
+          width: (0, i.so)(711),
+          ...(0, i.wz)({ fontSize: (0, i.jo)(24) })
+        }),
+        v = (0, r.Z)(c.RaleText)({
+          color: '#ffffff',
+          width: (0, i.so)(560),
+          textAlign: 'left',
+          ...(0, i.wz)({ fontSize: (0, i.jo)(16) })
+        });
+    },
+    750: function (e, n, t) {
+      'use strict';
+      t.r(n),
+        t.d(n, {
+          Button: function () {
+            return f;
+          }
+        });
+      var o = t(7437),
+        i = t(6566),
+        r = t.n(i),
+        c = t(7247),
+        s = t(8600),
+        l = t(1396),
+        a = t.n(l);
+      t(2265);
+      let f = (e) => {
+          let { children: n, href: t = '/#', ...i } = e;
+          return (0, o.jsx)(d, {
+            href: t,
+            'aria-label': 'buttons',
+            ...i,
+            children: (0, o.jsx)(u, { className: r().className, children: n })
+          });
+        },
+        d = (0, s.Z)(a())({
+          backgroundColor: '#FF1654',
+          width: (0, c.so)(240),
+          height: (0, c.Wk)(56),
+          borderRadius: (0, c.so)(4),
+          alignItems: 'center',
+          justifyContent: 'center',
+          display: 'inline-flex',
+          textDecoration: 'none',
+          ...(0, c.wz)({
+            width: (0, c.CD)(220),
+            height: (0, c.$s)(48),
+            borderRadius: (0, c.CD)(4)
+          })
+        }),
+        u = s.Z.span({
+          fontSize: (0, c.JB)(16),
+          color: '#ffffff',
+          display: 'inline-flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          margin: 0,
+          '& svg': { strokeWidth: (0, c.so)(24), height: (0, c.so)(24) },
+          ...(0, c.wz)({
+            fontSize: (0, c.jo)(16),
+            '& svg': { strokeWidth: (0, c.CD)(24), height: (0, c.CD)(24) }
+          })
+        });
+    },
+    6566: function (e) {
+      e.exports = {
+        style: {
+          fontFamily: "'__Manrope_30f681', '__Manrope_Fallback_30f681'",
+          fontWeight: 400,
+          fontStyle: 'normal'
+        },
+        className: '__className_30f681'
+      };
+    }
+  },
+  function (e) {
+    e.O(0, [641, 36, 971, 596, 744], function () {
+      return e((e.s = 5315));
+    }),
+      (_N_E = e.O());
+  }
+]);
