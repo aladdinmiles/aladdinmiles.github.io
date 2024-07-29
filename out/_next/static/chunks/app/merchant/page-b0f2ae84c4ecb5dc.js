44fe7603781cbe222f03bcee7a8de3ae08a25ba8
aladdinmiles <precious@aladdinmiles.com>
2024-07-29 14:17:55 +0100

    Deploy

--- out/_next/static/chunks/app/merchant/page-b0f2ae84c4ecb5dc.js
+++ out/_next/static/chunks/app/merchant/page-b0f2ae84c4ecb5dc.js
@@ -1,13 +1,13 @@
 (self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
   [872],
   {
-    6109: function (e, n, o) {
+    9e3: function (e, n, o) {
       Promise.resolve().then(o.t.bind(o, 3222, 23)),
         Promise.resolve().then(o.bind(o, 6773)),
         Promise.resolve().then(o.bind(o, 431)),
-        Promise.resolve().then(o.bind(o, 750)),
         Promise.resolve().then(o.bind(o, 3839)),
-        Promise.resolve().then(o.bind(o, 4914));
+        Promise.resolve().then(o.bind(o, 4914)),
+        Promise.resolve().then(o.bind(o, 750));
     },
     6566: function (e) {
       e.exports = {
@@ -22,7 +22,7 @@
   },
   function (e) {
     e.O(0, [641, 137, 36, 697, 971, 596, 744], function () {
-      return e((e.s = 6109));
+      return e((e.s = 9e3));
     }),
       (_N_E = e.O());
   }
